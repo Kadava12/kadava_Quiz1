68782720b6bdbfd755c592c218e39af5a4701073
chris <chris@192.168.3.108>
2023-09-04 16:22:17 +0800

    Quiz1

---
+++ src/kadava_quiz1/Kadava_Quiz1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package kadava_quiz1;
+
+/**
+ *
+ * @author chris
+ */
+public class Kadava_Quiz1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
